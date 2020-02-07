0802a98f5028de5096a872807c92d3ed426c7652
Kevin Blanco <kevjoseph.66@gmail.com>
2020-02-07 15:43:00 -0400

    Cambios en los textos de los cards

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,149 +1,165 @@
-import React, { Fragment, useState } from 'react';
-import nav from '../../../img/navbar.png'
-import Contact1 from '../../../img/contact1.png'
-import Contact2 from '../../../img/contact2.png'
-import './styles.css'
-import Phone from '../../../img/phone.png'
-import Message from '../../../img/message.png'
-import ubica from '../../../img/ubica.png'
-import { Redirect } from 'react-router-dom';
-import Axios from 'axios'
+import React, { Fragment, useState } from "react";
+import nav from "../../../img/navbar.png";
+import Contact1 from "../../../img/contact1.png";
+import Contact2 from "../../../img/contact2.png";
+import "./styles.css";
+import Phone from "../../../img/phone.png";
+import Message from "../../../img/message.png";
+import ubica from "../../../img/ubica.png";
+import { Redirect } from "react-router-dom";
+import Axios from "axios";
 
 // ES6 Modules or TypeScript
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 // CommonJS
 
+const index = props => {
+  const [errors, seterrors] = useState("");
 
-const index = (props) => {
-    
-    
-    
-    
-    
+  console.log("props.datos.email", props.datos.email);
 
-    const [errors, seterrors] = useState('')
+  const handleBlur = () => {
+    if (errors !== "") seterrors(props.datos);
+  };
 
-    console.log('props.datos.email', props.datos.email)
-    
-    
-    const handleBlur = () => {
-        if(errors !== '')
-        seterrors(props.datos)
-    }
-    
+  const handleSubmit = event => {
+    event.preventDefault();
 
-    const handleSubmit = (event) =>{
-        event.preventDefault()
+    seterrors(props.datos);
 
-           
-        
-            seterrors(props.datos)
-        
+    if (
+      props.datos.name !== "" &&
+      props.datos.phone !== "" &&
+      props.datos.email !== "" &&
+      props.datos.message !== ""
+    ) {
+      if (
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(props.datos.email)
+      ) {
+        console.log(
+          "La dirección de email " + props.datos.email + " es correcta."
+        );
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Invalid email!"
+        });
+      }
 
-        if (props.datos.name !== '' && props.datos.phone !== '' && props.datos.email !== '' && props.datos.message !== '' ){
-
-            if ( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(props.datos.email)){
-                console.log("La dirección de email " + props.datos.email + " es correcta.");
-               } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Invalid email!',
-                  })
-               }
-
-           
-        
-        Axios
-        .post('/enviar', props.datos, props.typesCards)
+      Axios.post("/enviar", props.datos, props.typesCards)
         .then(response => {
-          if(response.data.ok){
-            Swal.fire(
-                'Sended!',
-                'We will contact you',
-                'success',
-                
-
-              )
+          if (response.data.ok) {
+            Swal.fire("Sended!", "We will contact you", "success");
             props.setdatos({
-                name: '',
-                phone: '',
-                email: '',
-                message: '',
-                type: '' 
-            })
-            props.settypesCards('')
+              name: "",
+              phone: "",
+              email: "",
+              message: "",
+              type: ""
+            });
+            props.settypesCards("");
           }
-        }).catch(e => {
-            console.log('hubo error revisar network')
         })
-     } 
+        .catch(e => {
+          console.log("hubo error revisar network");
+        });
     }
+  };
 
+  return props.typesCards === "" ? (
+    <Redirect to="/" />
+  ) : (
+    <Fragment>
+      <div className="container">
+        <img src={nav} width="100%" className="navbar" alt="" />
+        <div className="bottom"></div>
 
-    return (
+        <div className="row-contact">
+          <img className="cimg" src={Contact1} width="22%" />
 
-        props.typesCards  === '' ? <Redirect to='/'/> :
-
-        <Fragment>
-        <div className='container'> 
-        
-            <img src={nav} width='100%' className='navbar' alt=""/>
-            <div className="bottom"></div>
-      
-    
-            <div className='row-contact'>
-
-            <img className='cimg' src={Contact1} width='22%'/>
-              
-                    <div className="c-contact">
-                        
-                        <div  className="title-contact">Contact us!</div>
-                        <div className="text-card">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, odio doloremque, vero eligendi harum dolorem minima cupiditate nobis modi rem expedita enim aliquam voluptatem ea. 
-                        </div>
-
-                        <form>
-                            <input onChange={e => {props.setdatos({...props.datos, name:e.target.value})}} type='text' required 
-                            onBlur={handleBlur}           placeholder='Name' id='name' />
-                            {errors.name === '' && <label>Please fill this field</label>}
-                            <input onBlur={handleBlur}  onChange={e => {props.setdatos({...props.datos, phone:e.target.value})}} type='number' required placeholder='Phone' id='tel'/>
-                            {errors.phone === '' && <label>Please fill this field</label>}
-                            <input onBlur={handleBlur}  onChange={e => {props.setdatos({...props.datos, email:e.target.value})}} type='email' required placeholder='Email' id='email' />
-                            {errors.email === '' && <label>Please fill this field</label>}
-                            <textarea onBlur={handleBlur}  onChange={e => {props.setdatos({...props.datos, message:e.target.value})}} placeholder='Comment' required rows="6" id='comment'/>
-                            {errors.message === '' && <label>Please fill this field</label>}
-                            <button onClick={handleSubmit} id='boton' >Send</button>
-                        </form>
-
-                        <div>
-
-                        <div className='icont'>
-                            <img src={Phone} width='20px' alt='#' />
-                            <div className="i-tex">proyectos ajenos</div>                      
-                        </div>
-
-                        <div className='icont'>
-                             <img src={Message} width='20px' alt='#' />
-                             <div className="i-tex">proyectos ajenos</div> 
-                        </div>
-
-                        <div className='icont'>
-                             <img src={ubica} width='20px' alt='#' />
-                             <div className="i-tex">proyectos ajenos</div> 
-                        </div>
-                        </div>
-
-                    </div>
-               
-
-                <img className='cimg' src={Contact2} width='22%'/> 
-
+          <div className="c-contact">
+            <div className="title-contact">Contact us!</div>
+            <div className="text-card">
+              If you are interested or want to know more about our services, do
+              not hesitate to contact us. Please leave your details and someone
+              from EYSS Medical will get back to you very soon.
             </div>
+
+            <form>
+              <input
+                onChange={e => {
+                  props.setdatos({ ...props.datos, name: e.target.value });
+                }}
+                type="text"
+                required
+                onBlur={handleBlur}
+                placeholder="Name"
+                id="name"
+              />
+              {errors.name === "" && <label>Please fill this field</label>}
+              <input
+                onBlur={handleBlur}
+                onChange={e => {
+                  props.setdatos({ ...props.datos, phone: e.target.value });
+                }}
+                type="number"
+                required
+                placeholder="Phone"
+                id="tel"
+              />
+              {errors.phone === "" && <label>Please fill this field</label>}
+              <input
+                onBlur={handleBlur}
+                onChange={e => {
+                  props.setdatos({ ...props.datos, email: e.target.value });
+                }}
+                type="email"
+                required
+                placeholder="Email"
+                id="email"
+              />
+              {errors.email === "" && <label>Please fill this field</label>}
+              <textarea
+                onBlur={handleBlur}
+                onChange={e => {
+                  props.setdatos({ ...props.datos, message: e.target.value });
+                }}
+                placeholder="Comment"
+                required
+                rows="6"
+                id="comment"
+              />
+              {errors.message === "" && <label>Please fill this field</label>}
+              <button onClick={handleSubmit} id="boton">
+                Send
+              </button>
+            </form>
+
+            <div>
+              {/* <div className="icont">
+                <img src={Phone} width="20px" alt="#" />
+                <div className="i-tex">proyectos ajenos</div>
+              </div> */}
+
+              <div className="icont">
+                <img src={Message} width="20px" alt="#" />
+                <div className="i-tex">info@medic.eyss.io</div>
+              </div>
+
+              {/* <div className="icont">
+                <img src={ubica} width="20px" alt="#" />
+                <div className="i-tex">proyectos ajenos</div>
+              </div> */}
+            </div>
+          </div>
+
+          <img className="cimg" src={Contact2} width="22%" />
         </div>
-        </Fragment>
-    );
+      </div>
+    </Fragment>
+  );
 };
 
 export default index;
--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,80 +1,83 @@
-import React, { Fragment } from 'react';
-import './styles.css';
-import nav from '../../../img/navbar.png'
-import Medical from '../../../img/medical.png'
-import Consultancy from '../../../img/consultancy.png'
-import landingimg from '../../../img/Landingimg.png'
-import Research from '../../../img/research.png'
-import { Redirect } from 'react-router-dom';
+import React, { Fragment } from "react";
+import "./styles.css";
+import nav from "../../../img/navbar.png";
+import Medical from "../../../img/medical.png";
+import Consultancy from "../../../img/consultancy.png";
+import landingimg from "../../../img/Landingimg.png";
+import Research from "../../../img/research.png";
+import { Redirect } from "react-router-dom";
 
-const index = (props) => {
+const index = props => {
+  const medical = async () => {
+    props.settypesCards("medical");
+  };
 
-    const medical = async () =>{
-        props.settypesCards('medical')
-    }
-    
-    const consultancy = () =>{
-        props.settypesCards('consultancy')
-    }
+  const consultancy = () => {
+    props.settypesCards("consultancy");
+  };
 
-    const research = () =>{
-        props.settypesCards('research')
-    }
-    
+  const research = () => {
+    props.settypesCards("research");
+  };
 
-    return (
+  return props.typesCards !== "" ? (
+    <Redirect to="/contact" />
+  ) : (
+    <Fragment>
+      <div className="container">
+        <img src={nav} width="100%" className="navbar" alt="" />
 
-        props.typesCards !== '' ? <Redirect to='/contact'/> : 
-        
-        <Fragment>
-        <div className='container'> 
-        
-            <img src={nav} width='100%' className='navbar' alt=""/>
-            
-      
-    
-            <div className='row'>
-                <div className='cards'>
-                    <div className="card">
-                        <img className='img-card' src={Medical} width='30%' />
-                        <p className='title-card'>Medical Consultation</p>
-                        <p className='text-card'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, consequatur? Laboriosam consectetur delectus soluta quaerat nam placeat.
-                        </p>
-                        <button className='btn-card' onClick={medical}>
-                            Lorem ipsum
-                        </button>
-                    </div>
-
-                    <div className="card">
-                        <img className='img-card' src={Consultancy} width='30%' />
-                        <p className='title-card'>Consultancy</p>
-                        <p className='text-card'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, consequatur? Laboriosam consectetur delectus soluta quaerat nam placeat.
-                        </p>
-                        <button className='btn-card' id='consultancy' name='consultancy' onClick={consultancy}>
-                            Lorem ipsum
-                        </button>
-                    </div>
-
-                    <div className="card">
-                        <img className='img-card' src={Research} width='30%' />
-                        <p className='title-card'>Research</p>
-                        <p className='text-card'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, consequatur? Laboriosam consectetur delectus soluta quaerat nam placeat.
-                        </p>
-                        <button className='btn-card' onClick={research}>
-                            Lorem ipsum
-                        </button>
-                    </div>
-                </div>
-
-                <img className='landing-img' src={landingimg} width='33%' />
+        <div className="row">
+          <div className="cards">
+            <div className="card">
+              <img className="img-card" src={Medical} width="30%" />
+              <p className="title-card">Medical Consultation</p>
+              <p className="text-card">
+                We provide primary care services of excellence to established
+                clinics in London. These includes, but not limited to, GP
+                consultations, Health screenings, Family planning, Travel Health
+                among others. to meet the long-term and short-term needs of our
+                clients around the clock.
+              </p>
+              <button className="btn-card" onClick={medical}>
+                View More
+              </button>
             </div>
+
+            <div className="card">
+              <img className="img-card" src={Consultancy} width="30%" />
+              <p className="title-card">Consultancy</p>
+              <p className="text-card">
+                Provide medical consultancy services to a range of companies,
+                start-ups and various Health projects. We strive to meet the
+                long-term and short-term needs of our clients around the clock.
+              </p>
+              <button className="btn-card" onClick={consultancy}>
+                View More
+              </button>
+            </div>
+
+            <div className="card">
+              <img className="img-card" src={Research} width="30%" />
+              <p className="title-card">Research</p>
+              <p className="text-card">
+                Due to our high commitment to clinical research we are
+                constantly looking to collaborate with innovative projects. We
+                work in partnership with our clients throughout the different
+                phases of the clinical research as well as support them with
+                medical writing.
+              </p>
+              <button className="btn-card" onClick={research}>
+                View More
+              </button>
+            </div>
+          </div>
+
+          <img className="landing-img" src={landingimg} width="33%" />
         </div>
-        </Fragment>
-        
-    );
+      </div>
+    </Fragment>
+  );
 };
 
 export default index;
